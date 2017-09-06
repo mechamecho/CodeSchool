92e10a4131943cc69438f4a2cdc7ccc7c9445f64
Livepurposefully <nimiko_chan@live.com>
2017-09-05 22:51:55 -0400

    Using socket.io

---
+++ realTimeWebWithNode/6Socket.io/video6/app.js
@@ -0,0 +1,18 @@
+var express=require('express');
+var app=express();
+var server=require('http').createServer(app);
+//dispatching request to express
+var io = require('socket.io')(server);
+//allowing the socket.io to use the http server to listen to a request
+//socket io and express are sharing the same http server
+io.on('connection', function(client){
+  console.log('Client connected ...');
+
+  //emitting the messages event on the client
+  client.emit('messages', {hello: 'world'});
+});
+app.get('/', function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+server.listen(8080);
